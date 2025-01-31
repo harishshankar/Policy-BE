6a335cb9f22517b8dd61a6b2a7c2d5abddd5bd85
harishsankar18 <btisankarhar@barytech.com>
2025-01-31 22:09:27 +0530

    db change

--- insurance_platform/settings.py
+++ insurance_platform/settings.py
@@ -80,17 +80,9 @@ WSGI_APPLICATION = "insurance_platform.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "sqlite": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "policy",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "localhost",
-        "PORT": "5432",
     },
 }
 
